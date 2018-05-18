b832cdaacbe1f4c16441761b9188c68a8466f5e5
ouromoros <ouromoros@gmail.com>
2018-05-18 14:26:34 +0800

    Fixed imports and cabal config

--- fibers-network/java/Utils.java
+++ fibers-network/java/Utils.java
@@ -2,6 +2,7 @@ package eta.contorl.concurrent.fiber.network;
 
 import java.nio.channels.Channel;
 import java.nio.channels.SocketChannel;
+import java.io.IOException;
 
 public class Utils{
     public static void setNonBlock(Channel c) throws IOException{
