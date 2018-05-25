5cf2cedd6d22725e6451edc47144601ce0689cf2
monser <ouromoros@gmail.com>
2018-05-25 22:55:15 +0800

    a working helloworld program

--- echo-server/Main.hs
+++ echo-server/Main.hs
@@ -4,10 +4,15 @@ module Main where
 import Control.Monad (unless)
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as B
+import Control.Concurrent (threadDelay)
 import Control.Concurrent.Fiber
 import Control.Concurrent.Fiber.Network
 import Control.Concurrent.Fiber.Network.Internal (fiber)
 
+-- This mesteriously would throw `ClosedByInterruptException`
+-- So maybe there are still some problems with concurrency
+-- main = forkFiber main' >> loop
+--   where loop = threadDelay 1000 >> loop
 main = fiber main'
 
 main' :: Fiber ()
@@ -16,13 +21,13 @@ main' = withSocketsDo $ do
     setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
     bind sock (SockAddrInet 4242 0)   -- listen on TCP port 4242.
     listen sock 2                              -- set a max of 2 queued connections
-    mainLoop sock                              
+    mainLoop sock
 
 
 mainLoop :: Socket -> Fiber ()
 mainLoop sock = do
     conn <- accept sock     -- accept a connection and handle it
-    runConn conn            -- run our server's logic
+    liftIO $ forkFiber $ runConn conn            -- run our server's logic
     mainLoop sock           -- repeat
  
 runConn :: (Socket, SockAddr) -> Fiber ()
